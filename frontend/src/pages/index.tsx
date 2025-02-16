82a8a543972e4b439b8fcf6521a0335a2f000f99
pablichoka <pablikoruab98@gmail.com>
2025-02-16 21:11:39 +0100

    Add language selector

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import styles from '@styles/Home.module.css';
 import Header from '@components/Header';
 import LoginForm from '@components/LoginForm';
+import Box from '@mui/material/Box';
+import LanguageSelector from '@components/LanguageSelector';
 
 function Home () {
   return (
@@ -15,12 +17,15 @@ function Home () {
     <div className={styles.container}>
       <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>Bienvenido a mi aplicación Next.js</h1>
-        <p className={styles.description}>
-          Comienza a construir tu aplicación aquí.
-        </p>
+      <h1 className={styles.title}>Bienvenido a mi aplicación Next.js</h1>
+      <p className={styles.description}>
+        Comienza a construir tu aplicación aquí.
+      </p>
       </main>
       <LoginForm />
+      <div className={styles.languageSelector}>
+      <LanguageSelector />
+      </div>
     </div>
     </>
   );
